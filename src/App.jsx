7e49ab364d9ec95026339d6c63d7fe72e79d9c47
naim195 <119221365+naim195@users.noreply.github.com>
2024-02-27 01:24:43 +0530

    Routing in Nav Bar Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import AboutUs from "./AboutUs";
+import Booklist from "./Booklist";
+import Profile from "./Profile";
+import Login from "./Login";
+import Register from "./Register";
+import Payment from "./Payment";
 
 function App() {
-  return(
-    <>
+  return (
+    <div className="main">
       <NavBar />
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about us" element={<AboutUs />} />
+        <Route path="/booklist" element={<Booklist />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/payment" element={<Payment />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,137 @@
-import React from "react";
-import { AppBar, Toolbar, IconButton, Typography, Stack } from "@mui/material";
-import IRCTCLogo from 'https://www.irctc.co.in/nget/assets/images/logo.png'; // Importing the local image
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
 
-export const NavBar = () => {
-  return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton aria-label="irctc-logo" color="inherit">
-            <img
-              src={IRCTCLogo}
-              alt="IRCTC Logo"
-              style={{ width: "40px", height: "40px" }}
-            />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </div>
+import { Link } from "react-router-dom";
+
+// Adding the IRCTCLogo component with the image
+const IRCTCLogo = () => (
+  <img
+    src="https://www.irctc.co.in/nget/assets/images/logo.png"
+    alt="IRCTC Logo"
+    style={{ height: "40px", width: "auto", marginRight: "50px"}}
+  />
+);
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
   );
-};
 
-export default NavBar;
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const navPages = [
+    "about us",
+    "booklist",
+    "profile",
+    "login",
+    "register",
+    "payment",
+  ];
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <IRCTCLogo />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            IRCTC
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="menu"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {navPages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">
+                    <Link
+                      to={`/${page}`}
+                      style={{ textDecoration: "none", color: "white" }}
+                    >
+                      {page}
+                    </Link>
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {navPages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                <Link
+                  style={{ textDecoration: "none", color: "white" }}
+                  to={`/${page}`}
+                >
+                  {page}
+                </Link>
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default ResponsiveAppBar;
