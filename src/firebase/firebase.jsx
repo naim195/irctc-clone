45e37230f2bfbc37fb54623f56275acb74e977db
naim195 <119221365+naim195@users.noreply.github.com>
2024-02-29 07:32:57 +0530

    changed firebase project

---
+++ src/firebase/firebase.jsx
@@ -0,0 +1,20 @@
+// firebase.jsx
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAz-LGDxDns-7YumXZCOh1rkfuVi5eKn64",
+  authDomain: "irctc-clone-df22b.firebaseapp.com",
+  projectId: "irctc-clone-df22b",
+  storageBucket: "irctc-clone-df22b.appspot.com",
+  messagingSenderId: "847051572226",
+  appId: "1:847051572226:web:68b097c0ef52fa6356d31b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
